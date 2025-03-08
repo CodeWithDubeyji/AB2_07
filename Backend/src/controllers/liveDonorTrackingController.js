927977c5995aef124f1573322e874bfc07548d0c
CodeWithDubeyji <dubeyj313@gmail.com>
2025-03-08 10:21:28 +0530

    live location controller

--- Backend/src/controllers/liveDonorTrackingController.js
+++ Backend/src/controllers/liveDonorTrackingController.js
@@ -25,7 +25,6 @@ const updateLocation = (req, res) => {
         return res.status(500).send('Socket.IO not initialized');
     }
 
-    // Emit the location update to all connected clients
     io.emit('locationUpdated', { userId, location });
     res.status(200).send({ userId, location });
 };
