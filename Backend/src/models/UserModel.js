5f1929738d48e8c0d5ea7125c45043f7700fa298
CodeWithDubeyji <dubeyj313@gmail.com>
2025-03-08 18:31:05 +0530

    updated UserModel

--- Backend/src/models/UserModel.js
+++ Backend/src/models/UserModel.js
@@ -11,7 +11,7 @@ const UserSchema = new mongoose.Schema({
   bloodType: { type: String },
   donated : {type : Number , defaut:0},
   request: {type : Number , default:0},
-  bloodType : {type : String , required:true},
+  
   password : {
     type:String,
     required:true
