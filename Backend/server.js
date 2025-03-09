b12c490fa05bd8da7a4c9823132930e725415866
CodeWithDubeyji <dubeyj313@gmail.com>
2025-03-09 23:04:28 +0530

    1. Trying to resolve the backend issue

--- Backend/server.js
+++ Backend/server.js
@@ -9,5 +9,5 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
